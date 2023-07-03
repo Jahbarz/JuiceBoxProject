acad3d044becd129160eb822163cb923aff3bcbc
TripolarHZ <hzha627@emory.edu>
2023-07-03 11:37:49 -0400

    new changes

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const { Client } = require('pg');
 
-const client = new Client('postgres://localhost:5432/juicebox-dev');
+const client = new Client('postgres://localhost:5432/juicebox');
 
 async function createUser({ username, password, name, location }) {
     try {
@@ -47,7 +47,47 @@ async function updateUser(/*id,*/ fields = {}) {
         throw error;
     }
 }
-console.log(3);
+
+async function createPost({
+    authorId,
+    title,
+    content
+  }) {
+    try {
+  
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async function updatePost(id, fields = {}) {
+    try {
+  
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async function getAllPosts() {
+    try {
+  
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async function getPostsByUser(userId) {
+    try {
+      const { rows } = await client.query(`
+        SELECT * FROM posts
+        WHERE "authorId"=$1;
+      `, [userId]);
+  
+      return rows;
+    } catch (error) {
+      throw error;
+    }
+  }
 
 module.exports = {
     client,
