4878771604227848df3aea33e1166d5e8ff92f62
Jahbarz <yveri95@gmail.com>
2023-07-04 15:34:59 -0400

    .

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const { Client } = require('pg');
 
-const client = new Client('postgres://localhost:5432/juicebox');
+const client = new Client('postgres://localhost:5432/juicebox-dev');
 
 async function createUser({
     username,
