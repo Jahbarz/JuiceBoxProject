7d5f3713d5211d830a53b325a56eb5fe0cb39f47
Jahbarz <yveri95@gmail.com>
2023-07-06 21:14:18 -0400

    part3

--- api/posts.js
+++ api/posts.js
@@ -1,5 +1,27 @@
 const express = require('express');
 const postsRouter = express.Router();
+const { createPost, getPostById, updatePost } = require('../db');
+
+const { requireUser } = require('./utils');
+
+postsRouter.post('/', requireUser, async (req, res, next) => {
+  const { title, content, tags = "" } = req.body;
+
+  const tagArr = tags.trim().split(/\s+/);
+  const postData = {
+    authorId: req.user.id, // Add the authorId from the authenticated user
+    title,
+    content,
+    tags: tagArr, // Assign the tag array to the postData object
+  };
+
+  try {
+    const post = await createPost(postData);
+    res.send({ post });
+  } catch (error) {
+    next(error);
+  }
+});
 
 postsRouter.use((req, res, next) => {
   console.log("A request is being made to /posts");
@@ -10,12 +32,80 @@ postsRouter.use((req, res, next) => {
 const { getAllPosts } = require('../db');
 
 
-postsRouter.get('/', async (req, res) => {
-  const posts = await getAllPosts();
+postsRouter.get('/', async (req, res, next) => {
+  try {
+    const allPosts = await getAllPosts();
 
-  res.send({
-    posts
-  });
+    const posts = allPosts.filter(post => {
+      // Keep a post if it is either active or if it belongs to the current user
+      return post.active || post.userId === req.currentUser.id;
+    });
+
+    res.send({
+      posts
+    });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+postsRouter.patch('/:postId', requireUser, async (req, res, next) => {
+  const { postId } = req.params;
+  const { title, content, tags } = req.body;
+
+  const updateFields = {};
+
+  if (tags && tags.length > 0) {
+    updateFields.tags = tags.trim().split(/\s+/);
+  }
+
+  if (title) {
+    updateFields.title = title;
+  }
+
+  if (content) {
+    updateFields.content = content;
+  }
+
+  try {
+    const originalPost = await getPostById(postId);
+
+    if (originalPost.author.id === req.user.id) {
+      const updatedPost = await updatePost(postId, updateFields);
+      res.send({ post: updatedPost })
+    } else {
+      next({
+        name: 'UnauthorizedUserError',
+        message: 'You cannot update a post that is not yours'
+      })
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+postsRouter.delete('/:postId', requireUser, async (req, res, next) => {
+  try {
+    const post = await getPostById(req.params.postId);
+
+    if (post && post.author.id === req.user.id) {
+      const updatedPost = await updatePost(post.id, { active: false });
+
+      res.send({ post: updatedPost });
+    } else {
+      // if there was a post, throw UnauthorizedUserError, otherwise throw PostNotFoundError
+      next(post ? { 
+        name: "UnauthorizedUserError",
+        message: "You cannot delete a post which is not yours"
+      } : {
+        name: "PostNotFoundError",
+        message: "That post does not exist"
+      });
+    }
+
+  } catch ({ name, message }) {
+    next({ name, message })
+  }
 });
 
 module.exports = postsRouter;
--- api/tags.js
+++ api/tags.js
@@ -7,15 +7,23 @@ tagsRouter.use((req, res, next) => {
   next();
 });
 
-const { getAllTags } = require('../db');
+const { getAllTags, getPostsByTagName } = require('../db');
 
 
-tagsRouter.get('/', async (req, res) => {
-  const tags = await getAllTags();
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+  // Read the tag name from the params
+  const { tagName } = req.params;
 
-  res.send({
-    tags
-  });
+  try {
+    // Use our method to get posts by tag name from the db
+    const posts = await getPostsByTagName({ tags: tagName });
+
+    // Send the response object to the client { posts: // the posts }
+    res.status(200).json({ posts });
+  } catch ({ name, message }) {
+    // Forward the name and message to the error handler
+    next({ name, message });
+  }
 });
 
 module.exports = tagsRouter;
